e8d2932fcc8aa679d1b0e00f4420704ee17e96ba
John Collier <jcollier@redhat.com>
2021-11-10 14:25:13 -0500

    Rename HASServiceDetectionQuery to HASComponentDetectionQuery
    Signed-off-by: John Collier <jcollier@redhat.com>

--- controllers/hascomponentdetectionquery_controller.go
+++ controllers/hascomponentdetectionquery_controller.go
@@ -27,26 +27,26 @@ import (
 	appstudiov1alpha1 "github.com/redhat-appstudio/application-service/api/v1alpha1"
 )
 
-// HASServiceDetectionQueryReconciler reconciles a HASServiceDetectionQuery object
-type HASServiceDetectionQueryReconciler struct {
+// HASComponentDetectionQueryReconciler reconciles a HASComponentDetectionQuery object
+type HASComponentDetectionQueryReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hasservicedetectionqueries,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hasservicedetectionqueries/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hasservicedetectionqueries/finalizers,verbs=update
+//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hascomponentdetectionqueries,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hascomponentdetectionqueries/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=hascomponentdetectionqueries/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
-// the HASServiceDetectionQuery object against the actual cluster state, and then
+// the HASComponentDetectionQuery object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
-func (r *HASServiceDetectionQueryReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+func (r *HASComponentDetectionQueryReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
 	// your logic here
@@ -55,8 +55,8 @@ func (r *HASServiceDetectionQueryReconciler) Reconcile(ctx context.Context, req 
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *HASServiceDetectionQueryReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *HASComponentDetectionQueryReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appstudiov1alpha1.HASServiceDetectionQuery{}).
+		For(&appstudiov1alpha1.HASComponentDetectionQuery{}).
 		Complete(r)
 }
