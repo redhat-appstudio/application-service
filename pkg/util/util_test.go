5dedf0a46dc6e7ac3882ad2a43fc3e6ee1e69cdb
Maysun J Faisal <maysunaneek@gmail.com>
2021-11-18 18:14:56 -0500

    Add test and update license
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -16,6 +16,7 @@
 package util
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
@@ -81,6 +82,177 @@ func TestGenerateNewRepositoryName(t *testing.T) {
 
 			if !strings.Contains(generatedRepo, sanitizedName) {
 				t.Errorf("TestSanitizeDisplayName() error: expected %v got %v", tt.want, sanitizedName)
+			}
+		})
+	}
+}
+
+func TestISExist(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		exist   bool
+		wantErr bool
+	}{
+		{
+			name:  "Path Exist",
+			path:  "/tmp",
+			exist: true,
+		},
+		{
+			name:  "Path Does Not Exist",
+			path:  "/pathdoesnotexist",
+			exist: false,
+		},
+		{
+			name:    "Error Case",
+			path:    "\000x",
+			exist:   false,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			isExist, err := IsExist(tt.path)
+			if tt.wantErr && (err == nil) {
+				t.Error("wanted error but got nil")
+			} else if !tt.wantErr && err != nil {
+				t.Errorf("got unexpected error %v", err)
+			} else if isExist != tt.exist {
+				t.Errorf("IsExist; expected %v got %v", tt.exist, isExist)
+			}
+		})
+	}
+}
+
+func TestCurlEndpoint(t *testing.T) {
+	tests := []struct {
+		name    string
+		url     string
+		wantErr bool
+	}{
+		{
+			name: "Valid Endpoint",
+			url:  "https://google.ca",
+		},
+		{
+			name:    "Invalid Endpoint",
+			url:     "https://google.ca/somepath",
+			wantErr: true,
+		},
+		{
+			name:    "Invalid URL",
+			url:     "\000x",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			contents, err := CurlEndpoint(tt.url)
+			if tt.wantErr && (err == nil) {
+				t.Error("wanted error but got nil")
+			} else if !tt.wantErr && err != nil {
+				t.Errorf("got unexpected error %v", err)
+			} else if err == nil && contents == nil {
+				t.Errorf("unable to read body")
+			}
+		})
+	}
+}
+
+func TestConvertGitHubURL(t *testing.T) {
+	tests := []struct {
+		name    string
+		url     string
+		wantUrl string
+		wantErr bool
+	}{
+		{
+			name:    "Successfully convert a github url to raw url",
+			url:     "https://github.com/devfile-samples/devfile-sample-java-springboot-basic",
+			wantUrl: "https://raw.githubusercontent.com/devfile-samples/devfile-sample-java-springboot-basic/main",
+		},
+		{
+			name:    "A non github url",
+			url:     "https://some.url",
+			wantUrl: "https://some.url",
+		},
+		{
+			name:    "A raw github url",
+			url:     "https://raw.githubusercontent.com/devfile-samples/devfile-sample-java-springboot-basic/main/devfile.yaml",
+			wantUrl: "https://raw.githubusercontent.com/devfile-samples/devfile-sample-java-springboot-basic/main/devfile.yaml",
+		},
+		{
+			name:    "A non-main branch github url",
+			url:     "https://github.com/devfile/api/tree/2.1.x",
+			wantUrl: "https://raw.githubusercontent.com/devfile/api/2.1.x",
+		},
+		{
+			name:    "A non url",
+			url:     "\000x",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			convertedUrl, err := ConvertGitHubURL(tt.url)
+			if tt.wantErr && (err == nil) {
+				t.Error("wanted error but got nil")
+			} else if !tt.wantErr && err != nil {
+				t.Errorf("got unexpected error %v", err)
+			} else if convertedUrl != tt.wantUrl {
+				t.Errorf("ConvertGitHubURL; expected %v got %v", tt.wantUrl, convertedUrl)
+			}
+		})
+	}
+}
+
+func TestCloneDevfileRepo(t *testing.T) {
+	tests := []struct {
+		name        string
+		clonePath   string
+		devfilePath string
+		repo        string
+		cloneTwice  bool
+		wantErr     bool
+	}{
+		{
+			name:        "Clone Successfully",
+			clonePath:   "/tmp/testclone",
+			devfilePath: "devfile.yaml",
+			repo:        "https://github.com/devfile-samples/devfile-sample-java-springboot-basic",
+			cloneTwice:  true,
+		},
+		{
+			name:    "Invalid Repo",
+			repo:    "https://invalid.url",
+			wantErr: true,
+		},
+		{
+			name:      "Invalid Clone Path",
+			clonePath: "\000x",
+			wantErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			devfileBytes, err := CloneAndReadDevfile(tt.clonePath, tt.devfilePath, tt.repo)
+			if tt.wantErr && (err == nil) {
+				t.Error("wanted error but got nil")
+			} else if !tt.wantErr && err != nil {
+				t.Errorf("got unexpected error %v", err)
+			} else if err == nil {
+				if len(devfileBytes) == 0 {
+					t.Error("devfile could not be read")
+				}
+				if tt.cloneTwice {
+					CloneAndReadDevfile(tt.clonePath, tt.devfilePath, tt.repo)
+				}
+				os.RemoveAll(tt.clonePath)
 			}
 		})
 	}
