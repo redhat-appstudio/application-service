a9f7315a5c8c706d92cd91daa3c87446b28f4016
Maysun J Faisal <maysunaneek@gmail.com>
2022-01-26 15:58:44 -0500

    Update check for enabling webhook
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- main.go
+++ main.go
@@ -154,14 +154,18 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ComponentDetectionQuery")
 		os.Exit(1)
 	}
-	if err = (&appstudiov1alpha1.Component{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Component")
-		os.Exit(1)
+
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&appstudiov1alpha1.Component{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Component")
+			os.Exit(1)
+		}
+		if err = (&appstudiov1alpha1.Application{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Application")
+			os.Exit(1)
+		}
 	}
-	if err = (&appstudiov1alpha1.Application{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Application")
-		os.Exit(1)
-	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
