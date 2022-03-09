dcaf0ab74889d175b7646ed8158c4af40daceb17
John Collier <jcollier@redhat.com>
2022-03-09 10:29:16 -0500

    Make some changes to improve code coverage (#51)
    * Make some changes to improve code coverage
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add unit test for git repo creation failure
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add test for invalid devfile.yaml in compdetectquery
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add component test for context folder with no devfile
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add some update.go test
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add a couple more update tests for edge cases
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Add stub unit tests for validatedelete
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Remove invalid test
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Tweak err checking
    
    Signed-off-by: John Collier <jcollier@redhat.com>

--- controllers/component_controller_test.go
+++ controllers/component_controller_test.go
@@ -1347,6 +1347,62 @@ var _ = Describe("Component controller", func() {
 		})
 	})
 
+	Context("Create Component with with context folder containing no devfile", func() {
+		It("Should error out because a devfile cannot be found", func() {
+			ctx := context.Background()
+
+			applicationName := HASAppName + "3"
+			componentName := HASCompName + "3"
+
+			createAndFetchSimpleApp(applicationName, HASAppNamespace, DisplayName, Description)
+
+			hasComp := &appstudiov1alpha1.Component{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "appstudio.redhat.com/v1alpha1",
+					Kind:       "Component",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      componentName,
+					Namespace: HASAppNamespace,
+				},
+				Spec: appstudiov1alpha1.ComponentSpec{
+					ComponentName: ComponentName,
+					Application:   applicationName,
+					Source: appstudiov1alpha1.ComponentSource{
+						ComponentSourceUnion: appstudiov1alpha1.ComponentSourceUnion{
+							GitSource: &appstudiov1alpha1.GitSource{
+								URL: "https://github.com/devfile-samples/devfile-sample-python-basic",
+							},
+						},
+					},
+					Context: "/docker",
+				},
+			}
+			Expect(k8sClient.Create(ctx, hasComp)).Should(Succeed())
+
+			// Look up the has app resource that was created.
+			// num(conditions) may still be < 1 on the first try, so retry until at least _some_ condition is set
+			hasCompLookupKey := types.NamespacedName{Name: componentName, Namespace: HASAppNamespace}
+			createdHasComp := &appstudiov1alpha1.Component{}
+			Eventually(func() bool {
+				k8sClient.Get(context.Background(), hasCompLookupKey, createdHasComp)
+				return len(createdHasComp.Status.Conditions) > 0
+			}, timeout, interval).Should(BeTrue())
+
+			// Make sure the err was set
+			Expect(createdHasComp.Status.Conditions[len(createdHasComp.Status.Conditions)-1].Reason).Should(Equal("Error"))
+			Expect(strings.ToLower(createdHasComp.Status.Conditions[len(createdHasComp.Status.Conditions)-1].Message)).Should(ContainSubstring("unable to find any devfiles in dir https:/raw.githubusercontent.com/devfile-samples/devfile-sample-python-basic/main/docker"))
+
+			hasAppLookupKey := types.NamespacedName{Name: applicationName, Namespace: HASAppNamespace}
+
+			// Delete the specified HASComp resource
+			deleteHASCompCR(hasCompLookupKey)
+
+			// Delete the specified HASApp resource
+			deleteHASAppCR(hasAppLookupKey)
+		})
+	})
+
 })
 
 type updateChecklist struct {
