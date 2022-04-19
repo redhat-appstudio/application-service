917a2cacb595a19c9da48fc4b2330adae0c3ede5
Mykola Morhun <mmorhun@redhat.com>
2022-04-19 10:26:06 +0300

    Delete creation of build objects
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- controllers/component_controller.go
+++ controllers/component_controller.go
@@ -31,7 +31,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/yaml"
 
@@ -46,7 +45,6 @@ import (
 	"github.com/redhat-appstudio/application-service/pkg/spi"
 	"github.com/redhat-appstudio/application-service/pkg/util"
 	"github.com/redhat-appstudio/application-service/pkg/util/ioutils"
-	triggersapi "github.com/tektoncd/triggers/pkg/apis/triggers/v1alpha1"
 
 	"github.com/spf13/afero"
 )
@@ -259,9 +257,7 @@ func (r *ComponentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 					return ctrl.Result{}, nil
 				}
 
-				log.Info(fmt.Sprintf("Creating the Build objects  %v", req.NamespacedName))
-				err = r.runBuild(ctx, &component)
-				r.SetCreateConditionAndUpdateCR(ctx, &component, err)
+				r.SetCreateConditionAndUpdateCR(ctx, &component, nil)
 			} else {
 				log.Error(err, fmt.Sprintf("Application devfile model is empty. Before creating a Component, an instance of Application should be created, exiting reconcile loop %v", req.NamespacedName))
 				err := fmt.Errorf("application devfile model is empty. Before creating a Component, an instance of Application should be created")
@@ -325,10 +321,6 @@ func (r *ComponentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 			component.Status.Devfile = string(yamlHASCompData)
 			r.SetUpdateConditionAndUpdateCR(ctx, &component, nil)
 
-			log.Info(fmt.Sprintf("Updating the Build objects  %v", req.NamespacedName))
-			err = r.runBuild(ctx, &component)
-			r.SetCreateConditionAndUpdateCR(ctx, &component, err)
-
 		} else {
 			log.Info(fmt.Sprintf("The Component devfile data was not updated %v", req.NamespacedName))
 		}
@@ -357,197 +349,6 @@ func (r *ComponentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	log.Info(fmt.Sprintf("Finished reconcile loop for %v", req.NamespacedName))
 	return ctrl.Result{}, nil
-}
-
-func (r *ComponentReconciler) runBuild(ctx context.Context, component *appstudiov1alpha1.Component) error {
-
-	log := r.Log.WithValues("Namespace", component.Namespace, "Application", component.Spec.Application, "Component", component.Name)
-
-	// TODO delete creation of gitops build objects(except PipelineRun) when build part of gitops repository will be respected
-
-	workspaceStorage := gitops.GenerateCommonStorage(*component, "appstudio")
-	pvc := &corev1.PersistentVolumeClaim{}
-	err := r.Get(ctx, types.NamespacedName{Name: workspaceStorage.Name, Namespace: workspaceStorage.Namespace}, pvc)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			err = r.Client.Create(ctx, &workspaceStorage)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Unable to create common storage %v", workspaceStorage))
-				return err
-			}
-		} else {
-			log.Error(err, fmt.Sprintf("Unable to get common storage %v", workspaceStorage))
-			return err
-		}
-	}
-	log.Info(fmt.Sprintf("PV is now present : %v", workspaceStorage.Name))
-
-	sourceSecretName := component.Spec.Source.GitSource.Secret
-
-	// Make the Secret ready for consumption by Tekton.
-	if sourceSecretName != "" {
-		gitSecret := corev1.Secret{}
-		err = r.Get(ctx, types.NamespacedName{Name: sourceSecretName, Namespace: component.Namespace}, &gitSecret)
-		if err != nil {
-			log.Error(err, fmt.Sprintf("Secret %s is missing", sourceSecretName))
-			return err
-		} else {
-			if gitSecret.Annotations == nil {
-				gitSecret.Annotations = map[string]string{}
-			}
-
-			gitHost, _ := getGitProvider(component.Spec.Source.GitSource.URL)
-
-			// doesn't matter if it was present, we will always override.
-			gitSecret.Annotations["tekton.dev/git-0"] = gitHost
-			err = r.Update(ctx, &gitSecret)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Secret %s  update failed", sourceSecretName))
-				return err
-			}
-		}
-	}
-
-	svcAccount := corev1.ServiceAccount{}
-	err = r.Get(ctx, types.NamespacedName{Name: "pipeline", Namespace: component.Namespace}, &svcAccount)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("OpenShift Pipelines-created Service account 'pipeline' is missing in namespace %s", component.Namespace))
-		return err
-	} else {
-		updatedRequired := updateServiceAccountIfSecretNotLinked(ctx, sourceSecretName, &svcAccount)
-		if updatedRequired {
-			err = r.Client.Update(ctx, &svcAccount)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Unable to update pipeline service account %v", svcAccount))
-				return err
-			}
-			log.Info(fmt.Sprintf("Service Account updated %v", svcAccount))
-		}
-	}
-
-	triggerTemplate, err := gitops.GenerateTriggerTemplate(*component)
-	if err != nil {
-		log.Error(err, "Unable to generate triggerTemplate ")
-		return err
-	}
-
-	err = controllerutil.SetOwnerReference(component, triggerTemplate, r.Scheme)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("Unable to set owner reference for %v", triggerTemplate))
-	}
-
-	existingTriggerTemplate := &triggersapi.TriggerTemplate{}
-	err = r.Get(ctx, types.NamespacedName{Name: triggerTemplate.Name, Namespace: triggerTemplate.Namespace}, existingTriggerTemplate)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			err = r.Client.Create(ctx, triggerTemplate)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Unable to create triggerTemplate %v", triggerTemplate))
-				return err
-			}
-			log.Info(fmt.Sprintf("TriggerTemplate created %v", triggerTemplate.Name))
-		} else {
-			log.Error(err, fmt.Sprintf("Unable to get triggerTemplate %s", triggerTemplate.Name))
-			return err
-		}
-	} else {
-		existingTriggerTemplate.Spec = triggerTemplate.Spec
-		err = r.Client.Update(ctx, existingTriggerTemplate)
-		if err != nil {
-			log.Error(err, fmt.Sprintf("Unable to update triggerTemplate %v", existingTriggerTemplate))
-			return err
-		}
-		log.Info(fmt.Sprintf("TriggerTemplate updated %v", triggerTemplate.Name))
-	}
-	eventListener := gitops.GenerateEventListener(*component, *triggerTemplate)
-
-	err = controllerutil.SetOwnerReference(component, &eventListener, r.Scheme)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("Unable to set owner reference for %v", eventListener))
-		return err
-	}
-	err = r.Get(ctx, types.NamespacedName{Name: eventListener.Name, Namespace: eventListener.Namespace}, &triggersapi.EventListener{})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			err = r.Client.Create(ctx, &eventListener)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Unable to create eventListener %v", eventListener))
-				return err
-			}
-		} else {
-			log.Error(err, fmt.Sprintf("Unable to get eventListener %v", eventListener))
-			return err
-		}
-	}
-
-	log.Info(fmt.Sprintf("Eventlistener created/updated %v", eventListener.Name))
-
-	initialBuild := gitops.GenerateInitialBuildPipelineRun(*component)
-	err = controllerutil.SetOwnerReference(component, &initialBuild, r.Scheme)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("Unable to set owner reference for %v", initialBuild))
-	}
-
-	err = r.Client.Create(ctx, &initialBuild)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("Unable to create the build PipelineRun %v", initialBuild))
-		return err
-	}
-
-	log.Info(fmt.Sprintf("Pipeline created %v", initialBuild))
-
-	webhook := gitops.GenerateBuildWebhookRoute(*component)
-
-	err = controllerutil.SetOwnerReference(component, &webhook, r.Scheme)
-	if err != nil {
-		log.Error(err, fmt.Sprintf("Unable to set owner reference for %v", webhook))
-	}
-
-	err = r.Get(ctx, types.NamespacedName{Name: webhook.Name, Namespace: webhook.Namespace}, &routev1.Route{})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			err = r.Client.Create(ctx, &webhook)
-			if err != nil {
-				log.Error(err, fmt.Sprintf("Unable to create webhook %v", webhook.Name))
-				return err
-			}
-		} else {
-			log.Error(err, fmt.Sprintf("Unable to get webhook %v", webhook.Name))
-			return err
-		}
-	}
-
-	return err
-}
-
-// getGitProvider takes a Git URL of the format https://github.com/foo/bar and returns
-// https://github.com
-func getGitProvider(gitURL string) (string, error) {
-	u, err := url.Parse(gitURL)
-
-	// We really need the format of the string to be correct.
-	// We'll not do any autocorrection.
-	if err != nil || u.Scheme == "" {
-		return "", fmt.Errorf("failed to parse string into a URL: %v or scheme is empty", err)
-	}
-	return u.Scheme + "://" + u.Host, nil
-}
-
-func updateServiceAccountIfSecretNotLinked(ctx context.Context, sourceSecretName string, serviceAccount *corev1.ServiceAccount) bool {
-	isSecretPresent := false
-	for _, credentialSecret := range serviceAccount.Secrets {
-		if credentialSecret.Name == sourceSecretName {
-			isSecretPresent = true
-			break
-		}
-	}
-	if !isSecretPresent {
-		serviceAccount.Secrets = append(serviceAccount.Secrets, corev1.ObjectReference{
-			Name: sourceSecretName,
-		})
-		return true
-	}
-	return false
 }
 
 // generateGitops retrieves the necessary information about a Component's gitops repository (URL, branch, context)
