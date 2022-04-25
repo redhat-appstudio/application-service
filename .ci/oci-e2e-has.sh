473c4c5a1a1fd0f6da5f66b953164a0e8bd19b86
flacatus <flacatus@redhat.com>
2022-04-25 12:48:33 +0200

    fix

--- .ci/oci-e2e-has.sh
+++ .ci/oci-e2e-has.sh
@@ -6,13 +6,20 @@ set -o pipefail
 # error on unset variables
 set -u
 
+if [ -z "${OPENSHIFT_CI}" ]; then
+    echo "[ERROR] The script is not running in openshift ci"
+    exit 1
+fi
+
+mkdir -p tmp/
+
 export ROOT_E2E="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 export WORKSPACE=${WORKSPACE:-${ROOT_E2E}}
 export E2E_CLONE_BRANCH="main"
 export E2E_REPO_LINK="https://github.com/redhat-appstudio/e2e-tests.git"
 
-mkdir -p tmp/
-
+# Example: CLONEREFS_OPTIONS={"src_root":"/go","log":"/dev/null","git_user_name":"ci-robot","git_user_email":"ci-robot@openshift.io","refs":[{"org":"redhat-appstudio","repo":"application-service","repo_link":"https://github.com/redhat-appstudio/application-service","base_ref":"main","base_sha":"75a4c79e49ab5c1a4c15d844256d1e4419da63e3","base_link":"https://github.com/redhat-appstudio/application-service/commit/75a4c79e49ab5c1a4c15d844256d1e4419da63e3","pulls":[{"number":91,"author":"flacatus","sha":"47b9fe555e27cc65c5ebfcf51c2d26a036fab235","link":"https://github.com/redhat-appstudio/application-service/pull/91","commit_link":"https://github.com/redhat-appstudio/application-service/pull/91/commits/47b9fe555e27cc65c5ebfcf51c2d26a036fab235","author_link":"https://github.com/flacatus"}]}],"fail":true}
+# Checking if CLONEREFS_OPTIONS openshift ci env exists and extract PR information to pair the PR
 if [[ -n ${CLONEREFS_OPTIONS} ]]; then
     AUTHOR=$(jq -r '.refs[0].pulls[0].author' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
     AUTHOR_LINK=$(jq -r '.refs[0].pulls[0].author_link' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
@@ -37,5 +44,4 @@ cd "$WORKSPACE"/tmp/e2e-tests
 make build
 chmod 755 "$WORKSPACE"/tmp/e2e-tests/bin/e2e-appstudio
 export PATH="$WORKSPACE"/tmp/e2e-tests/bin:${PATH}
-
-e2e-appstudio --help
+which e2e-appstudio
