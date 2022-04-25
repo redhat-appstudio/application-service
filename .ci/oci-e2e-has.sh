f818fdd6f04aa62f68d4d56e62fe02533068fb3c
flacatus <flacatus@redhat.com>
2022-04-25 10:32:19 +0200

    DO NOT MERGE DO NOT REVIEW

--- .ci/oci-e2e-has.sh
+++ .ci/oci-e2e-has.sh
@@ -6,9 +6,11 @@ set -o pipefail
 # error on unset variables
 
 if [[ -n ${CLONEREFS_OPTIONS} ]]; then
-    AUTHOR_LINK=$(jq -r '.refs[0].pulls[0].author_link' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
+    AUTHOR=$(jq -r '.refs[0].pulls[0].author' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
     GITHUB_ORGANIZATION=$(jq -r '.refs[0].org' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
-    GITHUB_HEAD_REPO=$(jq -r '.refs[0].repo' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
+    GITHUB_REPO=$(jq -r '.refs[0].repo' <<< ${CLONEREFS_OPTIONS} | tr -d '[:space:]')
 
-    curl https://api.github.com/repos/"${GITHUB_ORGANIZATION}"/"${GITHUB_HEAD_REPO}"/pulls/"${PULL_NUMBER}" | jq --raw-output .head.ref
+    PR_BRANCH_REF=$(curl https://api.github.com/repos/"${GITHUB_ORGANIZATION}"/"${GITHUB_REPO}"/pulls/"${PULL_NUMBER}" | jq --raw-output .head.ref)
+    curl https://api.github.com/repos/"${AUTHOR}"/e2e-tests/branches | jq '.[] | select(.name=="'${PR_BRANCH_REF}'")'
 fi
+
