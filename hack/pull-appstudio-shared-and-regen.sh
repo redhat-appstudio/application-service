19b8a13297541c5a51e0beffc367554bf8ef7e31
John Collier <jcollier@redhat.com>
2022-06-29 15:33:25 -0400

    Fix typo
    Signed-off-by: John Collier <jcollier@redhat.com>

--- hack/pull-appstudio-shared-and-regen.sh
+++ hack/pull-appstudio-shared-and-regen.sh
@@ -2,6 +2,6 @@
 
 # Simple script to pull the Appstudio Shared CRDs and regenerate the KCP API resources
 hackfolder="$(realpath $(dirname ${BASH_SOURCE[0]}))"
-curl -o $hackfolder/../config/crds/base/appstudio-shared-customresourcedefinitions.yaml https://raw.githubusercontent.com/redhat-appstudio/managed-gitops/main/appstudio-shared/manifests/appstudio-shared-customresourcedefinitions.yaml
+curl -o $hackfolder/../config/crd/bases/appstudio-shared-customresourcedefinitions.yaml https://raw.githubusercontent.com/redhat-appstudio/managed-gitops/main/appstudio-shared/manifests/appstudio-shared-customresourcedefinitions.yaml
 
 $hackfolder/generate-kcp-api.sh
