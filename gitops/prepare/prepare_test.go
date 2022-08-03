a115b03954150e9ca0b0f95bffeb9ad0de23927b
Mykola Morhun <mmorhun@redhat.com>
2022-08-03 14:25:31 +0300

    Update generate Pipelies-as-Code repository gitops function

--- gitops/prepare/prepare_test.go
+++ gitops/prepare/prepare_test.go
@@ -17,6 +17,7 @@ package prepare
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	appstudiov1alpha1 "github.com/redhat-appstudio/application-service/api/v1alpha1"
@@ -39,13 +40,14 @@ func TestPrepareGitopsConfig(t *testing.T) {
 		},
 	}
 	tests := []struct {
-		name string
-		data corev1.ConfigMap
-		want GitopsConfig
+		name                 string
+		buildBundleConfigMap corev1.ConfigMap
+		pacSecret            corev1.Secret
+		want                 GitopsConfig
 	}{
 		{
 			name: "should resolve the build bundle in case a configmap exists in the component's namespace",
-			data: corev1.ConfigMap{
+			buildBundleConfigMap: corev1.ConfigMap{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "v1",
 					Kind:       "ConfigMap",
@@ -58,23 +60,32 @@ func TestPrepareGitopsConfig(t *testing.T) {
 					Namespace: component.Namespace,
 				},
 			},
+			pacSecret: corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      PipelinesAsCodeSecretName,
+					Namespace: component.Namespace,
+				},
+				Data: map[string][]byte{
+					"github.token": []byte("ghp_token"),
+				},
+			},
 			want: GitopsConfig{
 				BuildBundle: "quay.io/foo/bar:1",
-			},
-		},
-		{
-			name: "should fall back to the hard-coded bundle in case the resolution fails",
-			data: corev1.ConfigMap{},
-			want: GitopsConfig{
-				BuildBundle: FallbackBuildBundle,
+				PipelinesAsCodeCredentials: map[string][]byte{
+					"github.token": []byte("ghp_token"),
+				},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client := fake.NewClientBuilder().WithRuntimeObjects(&tt.data).Build()
-			if got := PrepareGitopsConfig(context.TODO(), client, component); got != tt.want {
+			client := fake.NewClientBuilder().WithRuntimeObjects(&tt.buildBundleConfigMap, &tt.pacSecret).Build()
+			if got := PrepareGitopsConfig(context.TODO(), client, component); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("PrepareGitopsConfig() = %v, want %v", got, tt.want)
 			}
 		})
@@ -232,6 +243,62 @@ func TestResolveRegistrySecretPresence(t *testing.T) {
 
 			if got := resolveRegistrySecretPresence(ctx, client, component); got != tt.want {
 				t.Errorf("ResolveBuildBundle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestGetPipelinesAsCodeConfigurationSecretData(t *testing.T) {
+	ctx := context.TODO()
+
+	component := appstudiov1alpha1.Component{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "appstudio.redhat.com/v1alpha1",
+			Kind:       "Component",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "myName",
+			Namespace: "myNamespace",
+		},
+	}
+
+	tests := []struct {
+		name string
+		data *corev1.Secret
+		want map[string][]byte
+	}{
+		{
+			name: "secret exists",
+			data: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: component.Namespace,
+					Name:      PipelinesAsCodeSecretName,
+				},
+				Data: map[string][]byte{
+					"github.token": []byte("ghp_token"),
+				},
+			},
+			want: map[string][]byte{
+				"github.token": []byte("ghp_token"),
+			},
+		},
+		{
+			name: "secret does not exist",
+			want: map[string][]byte{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var client crclient.WithWatch
+			client = fake.NewClientBuilder().Build()
+			if tt.data != nil {
+				client = fake.NewClientBuilder().WithRuntimeObjects(tt.data).Build()
+			}
+
+			if got := getPipelinesAsCodeConfigurationSecretData(ctx, client, component); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
 			}
 		})
 	}
