55b9cc23a5c192d1db8d39e1c27a880edcbb6962
Mykola Morhun <mmorhun@redhat.com>
2022-12-19 16:08:30 +0200

    Remove Tekton trigger resources from gitops generation
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- gitops/generate_build.go
+++ gitops/generate_build.go
@@ -17,7 +17,6 @@ limitations under the License.
 package gitops
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 	"regexp"
@@ -26,22 +25,13 @@ import (
 	"github.com/redhat-developer/gitops-generator/pkg/resources"
 	"github.com/redhat-developer/gitops-generator/pkg/yaml"
 
-	devfilev1alpha2 "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
-	devfilecommon "github.com/devfile/library/pkg/devfile/parser/data/v2/common"
 	pacv1alpha1 "github.com/openshift-pipelines/pipelines-as-code/pkg/apis/pipelinesascode/v1alpha1"
-	routev1 "github.com/openshift/api/route/v1"
 	appstudiov1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
 	gitopsprepare "github.com/redhat-appstudio/application-service/gitops/prepare"
-	"github.com/redhat-appstudio/application-service/pkg/devfile"
 	"github.com/spf13/afero"
-	"github.com/tektoncd/pipeline/pkg/apis/pipeline/pod"
-	tektonapi "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
-	triggersapi "github.com/tektoncd/triggers/pkg/apis/triggers/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const (
@@ -72,29 +62,13 @@ func SetDefaultImageRepo(repo string) {
 }
 
 func GenerateBuild(fs afero.Fs, outputFolder string, component appstudiov1alpha1.Component, gitopsConfig gitopsprepare.GitopsConfig) error {
-	var buildResources map[string]interface{}
-	val, ok := component.Annotations[PaCAnnotation]
-	if (ok && val == "1") || gitopsConfig.IsHACBS {
-		repository, err := GeneratePACRepository(component, gitopsConfig.PipelinesAsCodeCredentials)
-		if err != nil {
-			return err
-		}
-		buildResources = map[string]interface{}{
-			buildRepositoryFileName: repository,
-		}
-	} else {
-		triggerTemplate, err := GenerateTriggerTemplate(component, gitopsConfig)
-		if err != nil {
-			return err
-		}
-		eventListener := GenerateEventListener(component, *triggerTemplate)
-		webhookRoute := GenerateBuildWebhookRoute(component)
+	repository, err := GeneratePACRepository(component, gitopsConfig.PipelinesAsCodeCredentials)
+	if err != nil {
+		return err
+	}
 
-		buildResources = map[string]interface{}{
-			buildTriggerTemplateFileName: triggerTemplate,
-			buildEventListenerFileName:   eventListener,
-			buildWebhookRouteFileName:    webhookRoute,
-		}
+	buildResources := map[string]interface{}{
+		buildRepositoryFileName: repository,
 	}
 
 	kustomize := resources.Kustomization{}
@@ -108,25 +82,6 @@ func GenerateBuild(fs afero.Fs, outputFolder string, component appstudiov1alpha1
 		return err
 	}
 	return nil
-}
-
-// GenerateInitialBuildPipelineRun generates pipeline run for initial build of the component.
-func GenerateInitialBuildPipelineRun(component appstudiov1alpha1.Component, gitopsConfig gitopsprepare.GitopsConfig) (tektonapi.PipelineRun, error) {
-	// normalizeOutputImageURL is not called with initial builds so we can ignore the error here
-	params, err := getParamsForComponentBuild(component, true)
-	if err != nil {
-		return tektonapi.PipelineRun{}, err
-	}
-	initialBuildSpec := DetermineBuildExecution(component, params, gitopsConfig)
-
-	return tektonapi.PipelineRun{
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: component.Name + "-",
-			Namespace:    component.Namespace,
-			Labels:       getBuildCommonLabelsForComponent(&component),
-		},
-		Spec: initialBuildSpec,
-	}, nil
 }
 
 // Generate volumeClaimTemplate for pipeline runs that requires their own PVC during run.
@@ -145,203 +100,6 @@ func GenerateVolumeClaimTemplate() *corev1.PersistentVolumeClaim {
 	}
 }
 
-// DetermineBuildExecution returns the pipelineRun spec that would be used
-// in webhooks-triggered pipelineRuns as well as user-triggered PipelineRuns
-func DetermineBuildExecution(component appstudiov1alpha1.Component, params []tektonapi.Param, gitopsConfig gitopsprepare.GitopsConfig) tektonapi.PipelineRunSpec {
-
-	pipelineRunSpec := tektonapi.PipelineRunSpec{
-		Params: params,
-		PipelineRef: &tektonapi.PipelineRef{
-			Name:   determineBuildPipeline(component),
-			Bundle: gitopsConfig.BuildBundle,
-		},
-
-		Workspaces: []tektonapi.WorkspaceBinding{
-			{
-				Name:                "workspace",
-				VolumeClaimTemplate: GenerateVolumeClaimTemplate(),
-			},
-		},
-	}
-	if gitopsConfig.AppStudioRegistrySecretPresent {
-		pipelineRunSpec.Workspaces = append(pipelineRunSpec.Workspaces,
-			tektonapi.WorkspaceBinding{
-				Name: "registry-auth",
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: gitopsprepare.RegistrySecret,
-				},
-			},
-		)
-		// imagePullSecret to be set in the podTemplate used by chains
-		pipelineRunSpec.PodTemplate = &pod.PodTemplate{
-			ImagePullSecrets: []corev1.LocalObjectReference{
-				{
-					Name: gitopsprepare.RegistrySecret,
-				},
-			},
-		}
-	}
-
-	return pipelineRunSpec
-}
-
-// determineBuildPipeline should detect build pipeline to use for the component and return its name.
-// If it fails to autodetect right pipeline, noop pipeline will be returned.
-// If a repository consists of two parts (e.g. frontend and backend), it should be mapped to two components (see context field in CR).
-// Available build pipeleines are located here: https://github.com/redhat-appstudio/build-definitions/tree/main/pipelines
-func determineBuildPipeline(component appstudiov1alpha1.Component) string {
-	// It is possible to skip error checks here because the model is propogated by component controller
-	componentDevfileData, err := devfile.ParseDevfileModel(component.Status.Devfile)
-	if err != nil {
-		// Return dummy value for tests
-		return "noop"
-	}
-
-	// Check for Dockerfile
-	filterOptions := devfilecommon.DevfileOptions{
-		ComponentOptions: devfilecommon.ComponentOptions{
-			ComponentType: devfilev1alpha2.ImageComponentType,
-		},
-	}
-	devfileComponents, _ := componentDevfileData.GetComponents(filterOptions)
-	for _, devfileComponent := range devfileComponents {
-		if devfileComponent.Image != nil && devfileComponent.Image.Dockerfile != nil {
-			return "docker-build"
-		}
-	}
-
-	// The only information about project is in language and projectType fileds under metadata of the devfile.
-	// They must be used to determine the right build pipeline.
-	devfileMetadata := componentDevfileData.GetMetadata()
-	language := devfileMetadata.Language
-	// TODO use projectType when build pipelines support frameworks
-	// projectType := devfileMetadata.ProjectType
-
-	switch strings.ToLower(language) {
-	case "java":
-		return "java-builder"
-	case "nodejs", "node":
-		return "nodejs-builder"
-	case "python":
-		// TODO return python-builder when the pipeline ready
-		return "noop"
-	}
-
-	// Failed to detect build pipeline
-	// Do nothing as we do not know how to build given component
-	return "noop"
-}
-
-func protectDefaultImageRepo(outputImage, namespace string) error {
-	// do not allow use of the default registry and a different user's tag
-	if strings.HasPrefix(outputImage, GetDefaultImageRepo()) {
-		if !strings.HasPrefix(outputImage, GetDefaultImageRepo()+":"+namespace+"-") || !strings.Contains(outputImage, ":") {
-			return fmt.Errorf("invalid user image tag combination of default repo %s and component namespace %s", outputImage, namespace)
-		}
-	}
-
-	return nil
-}
-
-func normalizeOutputImageURL(outputImage string) string {
-	// Check if the image has commit SHA suffix and delete it if so
-	shaSuffixRegExp := regexp.MustCompile(`(.+)-[0-9a-f]{40}$`)
-	foundImage := shaSuffixRegExp.FindSubmatch([]byte(outputImage))
-	if foundImage != nil {
-		// The outputImage matches regexp above and has format: image-sha1234
-		// Do not use the suffix in gitops repository.
-		outputImage = string(foundImage[1])
-	}
-
-	// If provided image has a tag, then append dash and git revision to it.
-	// Otherwise, use git revision as the tag.
-	// Examples:
-	//   quay.io/foo/bar:mytag ==> quay.io/foo/bar:mytag-git-revision
-	//   quay.io/foo/bar       ==> quay.io/foo/bar:latest-git-revision
-	if strings.Contains(outputImage, ":") {
-		outputImage = outputImage + "-" + "$(tt.params.git-revision)"
-	} else {
-		outputImage = outputImage + ":latest-" + "$(tt.params.git-revision)"
-	}
-	return outputImage
-}
-
-// getParamsForComponentBuild would return the 'input' parameters for the PipelineRun
-// that would build an image from source of the Component.
-// The key difference between webhook (regular) triggered PipelineRuns and user-triggered (initial) PipelineRuns
-// is that the git revision appended to the output image tag in case of webhook build.
-func getParamsForComponentBuild(component appstudiov1alpha1.Component, isInitialBuild bool) ([]tektonapi.Param, error) {
-	sourceCode := component.Spec.Source.GitSource.URL
-	revision := component.Spec.Source.GitSource.Revision
-	outputImage := component.Spec.ContainerImage
-	var err error
-
-	if err = protectDefaultImageRepo(outputImage, component.Namespace); err != nil {
-		return []tektonapi.Param{}, err
-	}
-
-	if !isInitialBuild {
-		outputImage = normalizeOutputImageURL(component.Spec.ContainerImage)
-	}
-
-	// Default required parameters
-	params := []tektonapi.Param{
-		{
-			Name: "git-url",
-			Value: tektonapi.ArrayOrString{
-				Type:      tektonapi.ParamTypeString,
-				StringVal: sourceCode,
-			},
-		},
-		{
-			Name: "output-image",
-			Value: tektonapi.ArrayOrString{
-				Type:      tektonapi.ParamTypeString,
-				StringVal: outputImage,
-			},
-		},
-	}
-	// if revision is specified in the component
-	// use it in the parms to the Pipeline Run
-	if revision != "" {
-		params = append(params, tektonapi.Param{
-			Name: "revision",
-			Value: tektonapi.ArrayOrString{
-				Type:      tektonapi.ParamTypeString,
-				StringVal: revision,
-			},
-		})
-	}
-	// Analyze component model for additional parameters
-	dockerfile, err := devfile.SearchForDockerfile([]byte(component.Status.Devfile))
-	if err != nil {
-		return []tektonapi.Param{}, err
-	}
-	if dockerfile != nil {
-		// Set dockerfile location
-		params = append(params, tektonapi.Param{
-			Name: "dockerfile",
-			Value: tektonapi.ArrayOrString{
-				Type:      tektonapi.ParamTypeString,
-				StringVal: dockerfile.Uri,
-			},
-		})
-
-		// Check for docker build context
-		if dockerfile.BuildContext != "" {
-			params = append(params, tektonapi.Param{
-				Name: "path-context",
-				Value: tektonapi.ArrayOrString{
-					Type:      tektonapi.ParamTypeString,
-					StringVal: dockerfile.BuildContext,
-				},
-			})
-		}
-	}
-
-	return params, err
-}
-
 func getBuildCommonLabelsForComponent(component *appstudiov1alpha1.Component) map[string]string {
 	labels := map[string]string{
 		"pipelines.appstudio.openshift.io/type": "build",
@@ -352,121 +110,6 @@ func getBuildCommonLabelsForComponent(component *appstudiov1alpha1.Component) ma
 		"appstudio.openshift.io/application":    component.Spec.Application,
 	}
 	return labels
-}
-
-// GenerateBuildWebhookRoute returns the Route resource that would enable
-// ingress traffic into the webhook endpoint ( aka EventListener)
-// TODO: This needs to be secure.
-func GenerateBuildWebhookRoute(component appstudiov1alpha1.Component) routev1.Route {
-	var port int32 = 8080
-	webhook := &routev1.Route{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Route",
-			APIVersion: "route.openshift.io/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        "el" + component.Name,
-			Namespace:   component.Namespace,
-			Annotations: getBuildCommonLabelsForComponent(&component),
-		},
-		Spec: routev1.RouteSpec{
-			Path: "/",
-			To: routev1.RouteTargetReference{
-				Kind: "Service",
-				Name: "el-" + component.Name,
-			},
-			Port: &routev1.RoutePort{
-				TargetPort: intstr.IntOrString{IntVal: port},
-			},
-		},
-	}
-	return *webhook
-}
-
-// GenerateTriggerTemplate generates the TriggerTemplate resources
-// which defines how a webhook-based trigger event would be handled -
-// In this case, a PipelineRun to build an image would be created.
-func GenerateTriggerTemplate(component appstudiov1alpha1.Component, gitopsConfig gitopsprepare.GitopsConfig) (*triggersapi.TriggerTemplate, error) {
-	params, err := getParamsForComponentBuild(component, false)
-	if err != nil {
-		return nil, err
-	}
-	webhookBasedBuildTemplate := DetermineBuildExecution(component, params, gitopsConfig)
-	resoureTemplatePipelineRun := tektonapi.PipelineRun{
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: component.Name + "-",
-			Namespace:    component.Namespace,
-			Annotations:  getBuildCommonLabelsForComponent(&component),
-			Labels:       getBuildCommonLabelsForComponent(&component),
-		},
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "PipelineRun",
-			APIVersion: "tekton.dev/v1beta1",
-		},
-		Spec: webhookBasedBuildTemplate,
-	}
-	resourceTemplatePipelineRunBytes, err := json.Marshal(resoureTemplatePipelineRun)
-	if err != nil {
-		return nil, err
-	}
-	triggerTemplate := triggersapi.TriggerTemplate{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "TriggerTemplate",
-			APIVersion: "triggers.tekton.dev/v1beta1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      component.Name,
-			Namespace: component.Namespace,
-		},
-		Spec: triggersapi.TriggerTemplateSpec{
-			Params: []triggersapi.ParamSpec{
-				{
-					Name: "git-revision",
-				},
-			},
-			ResourceTemplates: []triggersapi.TriggerResourceTemplate{
-				{
-					RawExtension: runtime.RawExtension{Raw: resourceTemplatePipelineRunBytes},
-				},
-			},
-		},
-	}
-	return &triggerTemplate, err
-}
-
-// The GenerateEventListener is responsible for defining how to "parse" the incoming event ( "github-push ")
-// and create the resultant PipelineRun ( defined as a TriggerTemplate ).
-// The reconciler for EventListeners create a Service, which when exposed enables
-// ingress traffic from Github events.
-func GenerateEventListener(component appstudiov1alpha1.Component, triggerTemplate triggersapi.TriggerTemplate) triggersapi.EventListener {
-	eventListener := triggersapi.EventListener{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "EventListener",
-			APIVersion: "triggers.tekton.dev/v1beta1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        component.Name,
-			Namespace:   component.Namespace,
-			Annotations: getBuildCommonLabelsForComponent(&component),
-		},
-		Spec: triggersapi.EventListenerSpec{
-			ServiceAccountName: "pipeline",
-			Triggers: []triggersapi.EventListenerTrigger{
-				{
-					Bindings: []*triggersapi.TriggerSpecBinding{
-						{
-							Ref:  "github-push",
-							Kind: triggersapi.ClusterTriggerBindingKind,
-						},
-					},
-					Template: &triggersapi.TriggerSpecTemplate{
-						Ref: &triggerTemplate.Name,
-					},
-				},
-			},
-		},
-	}
-	return eventListener
 }
 
 // GeneratePACRepository creates configuration of Pipelines as Code repository object.
@@ -534,17 +177,27 @@ func GetGitProvider(component appstudiov1alpha1.Component) (string, error) {
 	gitProvider := ""
 
 	sourceUrl := component.Spec.Source.GitSource.URL
-	u, err := url.Parse(sourceUrl)
-	if err != nil {
-		return "", err
-	}
-	uParts := strings.Split(u.Hostname(), ".")
-	if len(uParts) == 1 {
-		gitProvider = uParts[0]
+
+	if strings.HasPrefix(sourceUrl, "git@") {
+		// git@github.com:redhat-appstudio/application-service.git
+		sourceUrl = strings.TrimPrefix(sourceUrl, "git@")
+		host := strings.Split(sourceUrl, ":")[0]
+		gitProvider = strings.Split(host, ".")[0]
 	} else {
-		gitProvider = uParts[len(uParts)-2]
+		// https://github.com/redhat-appstudio/application-service
+		u, err := url.Parse(sourceUrl)
+		if err != nil {
+			return "", err
+		}
+		uParts := strings.Split(u.Hostname(), ".")
+		if len(uParts) == 1 {
+			gitProvider = uParts[0]
+		} else {
+			gitProvider = uParts[len(uParts)-2]
+		}
 	}
 
+	var err error
 	if !allowedGitProviders[gitProvider] {
 		// Self-hosted git provider, check for git-provider annotation on the component
 		gitProviderAnnotationValue := component.GetAnnotations()[GitProviderAnnotationName]
--- go.mod
+++ go.mod
@@ -24,8 +24,6 @@ require (
 	github.com/redhat-developer/gitops-generator v0.0.0-20221206221355-5678f9139ac8
 	github.com/spf13/afero v1.8.0
 	github.com/stretchr/testify v1.8.1
-	github.com/tektoncd/pipeline v0.42.0
-	github.com/tektoncd/triggers v0.19.1
 	go.uber.org/zap v1.23.0
 	golang.org/x/oauth2 v0.2.0
 	k8s.io/api v0.25.4
@@ -39,29 +37,24 @@ require (
 require (
 	cloud.google.com/go/compute v1.10.0 // indirect
 	code.gitea.io/sdk/gitea v0.15.1 // indirect
-	contrib.go.opencensus.io/exporter/ocagent v0.7.1-0.20200907061046-05415f1de66d // indirect
-	contrib.go.opencensus.io/exporter/prometheus v0.4.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest v0.11.28 // indirect
 	github.com/Azure/go-autorest/autorest/adal v0.9.21 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
-	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220418222510-f25a4f6275ed // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/bluekeyes/go-gitdiff v0.7.0 // indirect
-	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/containerd/containerd v1.6.10 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/go-kit/log v0.2.0 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -73,16 +66,13 @@ require (
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/btree v1.0.1 // indirect
-	github.com/google/cel-go v0.12.5 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/go-containerregistry v0.12.1 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
@@ -93,6 +83,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kcp-dev/apimachinery v0.0.0-20220803185518-868856d14e8a // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -113,20 +104,21 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
-	github.com/prometheus/statsd_exporter v0.21.0 // indirect
 	github.com/redhat-appstudio/service-provider-integration-operator v0.6.9 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06 // indirect
 	github.com/shurcooL/githubv4 v0.0.0-20190718010115-4ba037080260 // indirect
 	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/cobra v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stoewer/go-strcase v1.2.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/goleak v1.2.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
 	golang.org/x/mod v0.6.0 // indirect
@@ -137,7 +129,6 @@ require (
 	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/time v0.1.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
-	google.golang.org/api v0.100.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20221014213838-99cd37c6964a // indirect
 	google.golang.org/grpc v1.50.1 // indirect
