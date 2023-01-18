bed1ed0f9c84f890deb2d1bf2c40139dfc168696
John Collier <jcollier@redhat.com>
2023-01-18 18:29:47 -0500

    Fix typos in entrypoint
    Signed-off-by: John Collier <jcollier@redhat.com>

--- gitops-generator/entrypoint.sh
+++ gitops-generator/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+set -eux
 
 # Invoke the GitOps generator for the specified option
-if [ $OPERAION -eq "generate-base" ]; then
-    ./gitops-generator --operation $OPERATION --namespace $NAMESPACE --repoURL $REPOURL --component $RESOURCE --branch $BRANCH --context $CONTEXT
+if [ "$OPERATION" = "generate-base" ]; then
+    ./gitops-generator --operation "$OPERATION" --namespace "$NAMESPACE" --repoURL "$REPOURL" --component "$RESOURCE" --branch "$BRANCH" --path "$CONTEXT"
 else
-    ./gitops-generator --operation $OPERATION --namespace $NAMESPACE --repoURL $REPOURL --seb $RESOURCE 
+    ./gitops-generator --operation "$OPERATION" --namespace "$NAMESPACE" --repoURL "$REPOURL" --seb "$RESOURCE"
 fi
