78988fb6805a4da45179fc71fe90194f2aad5617
John Collier <jcollier@redhat.com>
2023-01-31 13:25:06 -0500

    Fix typo
    Signed-off-by: John Collier <jcollier@redhat.com>

--- controllers/componentdetectionquery_controller_test.go
+++ controllers/componentdetectionquery_controller_test.go
@@ -84,7 +84,7 @@ var _ = Describe("Component Detection Query controller", func() {
 			for devfileName, devfileDesc := range createdHasCompDetectionQuery.Status.ComponentDetected {
 				Expect(devfileName).Should(ContainSubstring("spring"))
 				Expect(devfileDesc.ComponentStub.Source.GitSource.Context).Should(ContainSubstring("./"))
-				Expect(devfileDesc.ComponentStub.Source.GitSource.Context).Should(ContainSubstring("main"))
+				Expect(devfileDesc.ComponentStub.Source.GitSource.Revision).Should(ContainSubstring("main"))
 				Expect(devfileDesc.ComponentStub.Source.GitSource.DevfileURL).Should(Equal("https://raw.githubusercontent.com/devfile-samples/devfile-sample-java-springboot-basic/main/devfile.yaml"))
 				Expect(devfileDesc.DevfileFound).Should(BeTrue())
 			}
