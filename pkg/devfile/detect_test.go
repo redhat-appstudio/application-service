4531e13c2e93bc083ef297fea3d08e6f8949e890
John Collier <jcollier@redhat.com>
2023-02-07 11:55:19 -0500

    Fix tests
    Signed-off-by: John Collier <jcollier@redhat.com>

--- pkg/devfile/detect_test.go
+++ pkg/devfile/detect_test.go
@@ -129,7 +129,7 @@ func TestSelectDevfileFromTypes(t *testing.T) {
 	}{
 		{
 			name:      "Successfully detect a devfile from the registry",
-			clonePath: "/tmp/java-springboot-basic",
+			clonePath: "/tmp/test-selected-devfile",
 			repo:      "https://github.com/maysunfaisal/devfile-sample-java-springboot-basic-1",
 			devfileTypes: []model.DevFileType{
 				{
@@ -158,8 +158,8 @@ func TestSelectDevfileFromTypes(t *testing.T) {
 		},
 		{
 			name:      "Unable to detect a devfile from the registry",
-			clonePath: "/tmp/test-console",
-			repo:      "https://github.com/devfile-resources/go-vendor-alizer-no-detection",
+			clonePath: "/tmp/test-no-devfiles-selected",
+			repo:      "https://github.com/maysunfaisal/devfile-sample-java-springboot-basic-1",
 			devfileTypes: []model.DevFileType{
 				{
 					Name: "python-basic", Language: "Python", ProjectType: "Python", Tags: []string{"Python", "Pip", "Flask"},
