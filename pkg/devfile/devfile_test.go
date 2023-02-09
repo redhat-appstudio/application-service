ba9d321e3221ce829d9f0c4d798b8bc7174958ca
Maysun J Faisal <maysunaneek@gmail.com>
2023-02-08 19:30:11 -0500

    Only add ports if not present
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- pkg/devfile/devfile_test.go
+++ pkg/devfile/devfile_test.go
@@ -615,6 +615,8 @@ func TestGenerateDeploymentTemplate(t *testing.T) {
 
 func TestGetResourceFromDevfile(t *testing.T) {
 
+	weight := int32(100)
+
 	kubernetesInlinedDevfile := `
 commands:
 - apply:
@@ -988,17 +990,14 @@ components:
     deployment/storageRequest: 201Mi
   kubernetes:
     deployByDefault: false
-    endpoints:
-    - name: http-8081
-      path: /
-      secure: false
-      targetPort: 8081
     inlined: |-
       apiVersion: route.openshift.io/v1
       kind: Route
       metadata:
         creationTimestamp: null
         name: route-sample-2
+        labels:
+          test: test
       spec:
         host: route111
         port:
@@ -1044,6 +1043,7 @@ components:
   name: image-build
 - attributes:
     api.devfile.io/k8sLikeComponent-originalURI: deploy.yaml
+    deployment/container-port: 1111
   kubernetes:
     deployByDefault: false
     inlined: |-
@@ -1056,8 +1056,6 @@ components:
         ports:
         - port: 1111
           targetPort: 1111
-        selector:
-          app.kubernetes.io/instance: component-sample
       status:
         loadBalancer: {}
   name: kubernetes-deploy
@@ -1093,6 +1091,9 @@ components:
   name: image-build
 - attributes:
     api.devfile.io/k8sLikeComponent-originalURI: deploy.yaml
+    deployment/container-port: 1111
+    deployment/storageLimit: 401Mi
+    deployment/storageRequest: 201Mi
   kubernetes:
     deployByDefault: false
     endpoints:
@@ -1105,13 +1106,6 @@ components:
       kind: Deployment
       metadata:
         creationTimestamp: null
-        labels:
-          app.kubernetes.io/created-by: application-service
-          app.kubernetes.io/instance: component-sample
-          app.kubernetes.io/managed-by: kustomize
-          app.kubernetes.io/name: backend
-          app.kubernetes.io/part-of: application-sample
-          maysun: test
         name: deploy-sample
       spec:
         replicas: 1
@@ -1796,18 +1790,22 @@ schemaVersion: 2.2.0`
 						"app.kubernetes.io/managed-by": "kustomize",
 						"app.kubernetes.io/name":       "component-sample",
 						"app.kubernetes.io/part-of":    "application-sample",
+						"test":                         "test",
 					},
-					Annotations: map[string]string{},
 				},
 				Spec: routev1.RouteSpec{
 					Host: "route111222",
-					Path: "/",
 					Port: &routev1.RoutePort{
 						TargetPort: intstr.FromInt(5566),
 					},
 					To: routev1.RouteTargetReference{
-						Kind: "Service",
-						Name: "component-sample",
+						Kind:   "Service",
+						Name:   "component-sample",
+						Weight: &weight,
+					},
+					TLS: &routev1.TLSConfig{
+						Termination:                   routev1.TLSTerminationEdge,
+						InsecureEdgeTerminationPolicy: routev1.InsecureEdgeTerminationPolicyRedirect,
 					},
 				},
 			},
@@ -1867,7 +1865,6 @@ schemaVersion: 2.2.0`
 						"app.kubernetes.io/managed-by": "kustomize",
 						"app.kubernetes.io/name":       "component-sample",
 						"app.kubernetes.io/part-of":    "application-sample",
-						"maysun":                       "test",
 					},
 				},
 				Spec: appsv1.DeploymentSpec{
@@ -1927,12 +1924,12 @@ schemaVersion: 2.2.0`
 										Limits: corev1.ResourceList{
 											corev1.ResourceCPU:     resource.MustParse("2"),
 											corev1.ResourceMemory:  resource.MustParse("500Mi"),
-											corev1.ResourceStorage: resource.MustParse("400Mi"),
+											corev1.ResourceStorage: resource.MustParse("401Mi"),
 										},
 										Requests: corev1.ResourceList{
 											corev1.ResourceCPU:     resource.MustParse("700m"),
 											corev1.ResourceMemory:  resource.MustParse("400Mi"),
-											corev1.ResourceStorage: resource.MustParse("200Mi"),
+											corev1.ResourceStorage: resource.MustParse("201Mi"),
 										},
 									},
 								},
@@ -1961,7 +1958,7 @@ schemaVersion: 2.2.0`
 				Spec: routev1.RouteSpec{
 					Path: "/",
 					Port: &routev1.RoutePort{
-						TargetPort: intstr.FromString("8081"),
+						TargetPort: intstr.FromInt(1111),
 					},
 					To: routev1.RouteTargetReference{
 						Kind: "Service",
