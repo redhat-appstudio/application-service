6d23b1d8c53c195b3db882d84d8fca197fbeb8d4
Stephanie Cao <yangcao@redhat.com>
2023-03-22 12:56:18 +0800

    fix tailing forward slash (#284)
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- controllers/componentdetectionquery_controller.go
+++ controllers/componentdetectionquery_controller.go
@@ -128,12 +128,17 @@ func (r *ComponentDetectionQueryReconciler) Reconcile(ctx context.Context, req c
 		if context == "" {
 			context = "./"
 		}
+		sourceURL := source.URL
+		// If the repository URL ends in a forward slash, remove it to avoid issues with default branch lookup
+		if string(sourceURL[len(sourceURL)-1]) == "/" {
+			sourceURL = sourceURL[0 : len(sourceURL)-1]
+		}
 		if source.Revision == "" {
 			log.Info(fmt.Sprintf("Look for default branch of repo %s... %v", source.URL, req.NamespacedName))
-			source.Revision, err = github.GetDefaultBranchFromURL(source.URL, r.GitHubClient, ctx)
+			source.Revision, err = github.GetDefaultBranchFromURL(sourceURL, r.GitHubClient, ctx)
 			if err != nil {
 				log.Error(err, fmt.Sprintf("Unable to get default branch of Github Repo %v, try to fall back to main branch... %v", source.URL, req.NamespacedName))
-				_, err := github.GetBranchFromURL(source.URL, r.GitHubClient, ctx, "main")
+				_, err := github.GetBranchFromURL(sourceURL, r.GitHubClient, ctx, "main")
 				if err != nil {
 					log.Error(err, fmt.Sprintf("Unable to get main branch of Github Repo %v ... %v", source.URL, req.NamespacedName))
 					retErr := fmt.Errorf("unable to get default branch of Github Repo %v, try to fall back to main branch, failed to get main branch... %v", source.URL, req.NamespacedName)
