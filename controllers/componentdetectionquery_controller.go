c195f659a66ee9ce8a9e528722825903b3ec6562
Stephanie <yangcao@redhat.com>
2023-05-10 16:22:45 +0800

    update the revision in cdq spec in order to return it
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- controllers/componentdetectionquery_controller.go
+++ controllers/componentdetectionquery_controller.go
@@ -168,6 +168,8 @@ func (r *ComponentDetectionQueryReconciler) Reconcile(ctx context.Context, req c
 				}
 			}
 		}
+		// set in the CDQ spec
+		componentDetectionQuery.Spec.GitSource.Revision = source.Revision
 
 		if source.DevfileURL == "" {
 			isMultiComponent := false
