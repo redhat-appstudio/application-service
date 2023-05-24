6f2e0245c80e265413cf432a2dcfc2c84ba9cb26
Katka <kkanova@redhat.com>
2023-05-24 16:27:55 +0200

    Install pact standalone and refactor tests

--- controllers/application_pact_test.go
+++ controllers/application_pact_test.go
@@ -30,24 +30,58 @@ import (
 	"github.com/pact-foundation/pact-go/dsl"
 	pactTypes "github.com/pact-foundation/pact-go/types"
 	appstudiov1alpha1 "github.com/redhat-appstudio/application-api/api/v1alpha1"
-
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestContracts(t *testing.T) {
+	// setup default variables for tests
+	HASAppNamespace := "default"
 
-	if os.Getenv("PACT_BROKER_USERNAME") == "" {
-		t.Skip("Skipping PACT tests as credentials are not set.")
+	// setup default variables for Pact
+	selector := pactTypes.ConsumerVersionSelector{Branch: "main"}
+	verifyRequest := pactTypes.VerifyRequest{
+		ProviderVersion:            os.Getenv("COMMIT_SHA"),
+		ConsumerVersionSelectors:   []pactTypes.ConsumerVersionSelector{selector},
+		BrokerURL:                  "https://pact-broker-hac-pact-broker.apps.hac-devsandbox.5unc.p1.openshiftapps.com",
+		Verbose:                    true,
+		PublishVerificationResults: false,
 	}
 
+	// setup credentianls and publishing
+	if os.Getenv("PR_CHECK") == "true" {
+		if os.Getenv("PACT_BROKER_USERNAME") == "" {
+			t.Skip("Skipping Pact tests from unit test suite during PR check.")
+		} else {
+			t.Log("Running Pact tests from a PR check.")
+			verifyRequest.BrokerUsername = os.Getenv("PACT_BROKER_USERNAME")
+			verifyRequest.BrokerPassword = os.Getenv("PACT_BROKER_PASSWORD")
+			verifyRequest.PublishVerificationResults = true
+			verifyRequest.ProviderTags = []string{"PR" + os.Getenv("PR_NUMBER")}
+		}
+	} else {
+		if os.Getenv("PACT_BROKER_USERNAME") == "" {
+			verifyRequest.BrokerUsername = "pactCommonUser"
+			verifyRequest.BrokerPassword = "pactCommonPassword123"
+			verifyRequest.ProviderVersion = "local"
+			t.Log("Running tests locally. Setting up default creds.")
+
+			// For running localy and testing Pacts from a locall directory:
+			//  - uncomment pactDir variable and set it to the folder with pacts
+			//  - uncomment verifyRequest.PactURLs
+			//  - comment out all Broker* variables (BrokerUsername, BrokerPassword, BrokerURL)
+			// var pactDir = "/home/us/pacts"
+			// verifyRequest.PactURLs = []string{filepath.ToSlash(fmt.Sprintf("%s/HACdev-HAS.json", pactDir))}
+		} else {
+			t.Skip("Running tests locally with Pact User setup.")
+		}
+	}
+
+	// Register fail handler and setup test environment (same as during unit tests)
 	RegisterFailHandler(Fail)
 	setupTestEnv()
 
-	pact := &dsl.Pact{
-		Provider: "HAS",
-	}
-	pact.LogLevel = "TRACE"
+	verifyRequest.ProviderBaseURL = testEnv.Config.Host
 
 	// Certificate magic - for the mocked service to be able to communicate with kube-apiserver & for authorization
 	caCertPool := x509.NewCertPool()
@@ -60,93 +94,84 @@ func TestContracts(t *testing.T) {
 		RootCAs:      caCertPool,
 		Certificates: []tls.Certificate{certs},
 	}
+	verifyRequest.CustomTLSConfig = tlsConfig
 	// End of certificate magic
 
-	// uncomment for local development and setup path to your pact files folder
-	// var pactDir = "/home/us/pacts"
-	selector := pactTypes.ConsumerVersionSelector{
-		Latest: true,
-	}
-	HASAppNamespace := "default"
-	_, err = pact.VerifyProvider(t, pactTypes.VerifyRequest{
-		ProviderVersion:          "1.0.1",
-		ConsumerVersionSelectors: []pactTypes.ConsumerVersionSelector{selector},
-		ProviderBaseURL:          testEnv.Config.Host,
-		// uncomment for local development, comment out all Broker* variables
-		// PactURLs:        []string{filepath.ToSlash(fmt.Sprintf("%s/hacdev-has.json", pactDir))},
-		BrokerURL:                  "https://pact-broker-hac-pact-broker.apps.hac-devsandbox.5unc.p1.openshiftapps.com",
-		BrokerUsername:             os.Getenv("PACT_BROKER_USERNAME"),
-		BrokerPassword:             os.Getenv("PACT_BROKER_PASSWORD"),
-		CustomTLSConfig:            tlsConfig,
-		Verbose:                    true,
-		PublishVerificationResults: true,
-		StateHandlers: pactTypes.StateHandlers{
-			"No app with the name myapp in the default namespace exists.": func() error {
-				return nil
-			},
-			"App myapp exists and has component gh-component and quay-component": func() error {
-				appName := "myapp"
-				ghCompName := "gh-component"
-				quayCompName := "quay-component"
-				ghCompRepoLink := "https://github.com/devfile-samples/devfile-sample-java-springboot-basic"
-				quayRepoLink := "quay.io/test/test-image:latest"
-
-				hasApp := getApplicationSpec(appName, HASAppNamespace)
-				ghComp := getGhComponentSpec(ghCompName, HASAppNamespace, appName, ghCompRepoLink)
-				quayComp := getQuayComponentSpec(quayCompName, HASAppNamespace, appName, quayRepoLink)
-
-				k8sClient.Create(ctx, hasApp)
-				hasAppLookupKey := types.NamespacedName{Name: appName, Namespace: HASAppNamespace}
-				createdHasApp := &appstudiov1alpha1.Application{}
-				for i := 0; i < 12; i++ {
-					k8sClient.Get(context.Background(), hasAppLookupKey, createdHasApp)
-					if len(createdHasApp.Status.Conditions) > 0 {
-						if createdHasApp.Status.Conditions[0].Type == "Created" {
-							break
-						}
-					}
-					time.Sleep(10 * time.Second)
-				}
-
-				k8sClient.Create(ctx, ghComp)
-				hasCompLookupKey := types.NamespacedName{Name: ghCompName, Namespace: HASAppNamespace}
-				createdHasComp := &appstudiov1alpha1.Component{}
-				for i := 0; i < 12; i++ {
-					k8sClient.Get(context.Background(), hasCompLookupKey, createdHasComp)
-					if len(createdHasComp.Status.Conditions) > 1 {
-						break
-					}
-					time.Sleep(10 * time.Second)
-				}
-
-				k8sClient.Create(ctx, quayComp)
-				hasCompLookupKey2 := types.NamespacedName{Name: quayCompName, Namespace: HASAppNamespace}
-				createdHasComp2 := &appstudiov1alpha1.Component{}
-				for i := 0; i < 12; i++ {
-					k8sClient.Get(context.Background(), hasCompLookupKey2, createdHasComp2)
-					if len(createdHasComp2.Status.Conditions) > 1 {
-						break
-					}
-					time.Sleep(10 * time.Second)
-				}
-
-				for i := 0; i < 12; i++ {
-					k8sClient.Get(context.Background(), hasAppLookupKey, createdHasApp)
-					if len(createdHasApp.Status.Conditions) > 0 && strings.Contains(createdHasApp.Status.Devfile, ghCompName) {
-						break
-					}
-					time.Sleep(10 * time.Second)
-				}
-				return nil
-			},
-		},
-		AfterEach: func() error {
-			//Remove all applications and components after each tests
-			k8sClient.DeleteAllOf(context.Background(), &appstudiov1alpha1.Application{}, client.InNamespace(HASAppNamespace))
-			k8sClient.DeleteAllOf(context.Background(), &appstudiov1alpha1.Component{}, client.InNamespace(HASAppNamespace))
+	// setup state handlers
+	verifyRequest.StateHandlers = pactTypes.StateHandlers{
+		"No app with the name myapp in the default namespace exists.": func() error {
 			return nil
 		},
-	})
+		"App myapp exists and has component gh-component and quay-component": func() error {
+			appName := "myapp"
+			ghCompName := "gh-component"
+			quayCompName := "quay-component"
+			ghCompRepoLink := "https://github.com/devfile-samples/devfile-sample-java-springboot-basic"
+			quayRepoLink := "quay.io/test/test-image:latest"
+
+			hasApp := getApplicationSpec(appName, HASAppNamespace)
+			ghComp := getGhComponentSpec(ghCompName, HASAppNamespace, appName, ghCompRepoLink)
+			quayComp := getQuayComponentSpec(quayCompName, HASAppNamespace, appName, quayRepoLink)
+
+			k8sClient.Create(ctx, hasApp)
+			hasAppLookupKey := types.NamespacedName{Name: appName, Namespace: HASAppNamespace}
+			createdHasApp := &appstudiov1alpha1.Application{}
+			for i := 0; i < 12; i++ {
+				k8sClient.Get(context.Background(), hasAppLookupKey, createdHasApp)
+				if len(createdHasApp.Status.Conditions) > 0 {
+					if createdHasApp.Status.Conditions[0].Type == "Created" {
+						break
+					}
+				}
+				time.Sleep(10 * time.Second)
+			}
+
+			k8sClient.Create(ctx, ghComp)
+			hasCompLookupKey := types.NamespacedName{Name: ghCompName, Namespace: HASAppNamespace}
+			createdHasComp := &appstudiov1alpha1.Component{}
+			for i := 0; i < 12; i++ {
+				k8sClient.Get(context.Background(), hasCompLookupKey, createdHasComp)
+				if len(createdHasComp.Status.Conditions) > 1 {
+					break
+				}
+				time.Sleep(10 * time.Second)
+			}
+
+			k8sClient.Create(ctx, quayComp)
+			hasCompLookupKey2 := types.NamespacedName{Name: quayCompName, Namespace: HASAppNamespace}
+			createdHasComp2 := &appstudiov1alpha1.Component{}
+			for i := 0; i < 12; i++ {
+				k8sClient.Get(context.Background(), hasCompLookupKey2, createdHasComp2)
+				if len(createdHasComp2.Status.Conditions) > 1 {
+					break
+				}
+				time.Sleep(10 * time.Second)
+			}
+
+			for i := 0; i < 12; i++ {
+				k8sClient.Get(context.Background(), hasAppLookupKey, createdHasApp)
+				if len(createdHasApp.Status.Conditions) > 0 && strings.Contains(createdHasApp.Status.Devfile, ghCompName) {
+					break
+				}
+				time.Sleep(10 * time.Second)
+			}
+			return nil
+		},
+	}
+	verifyRequest.AfterEach = func() error {
+		//Remove all applications and components after each tests
+		k8sClient.DeleteAllOf(context.Background(), &appstudiov1alpha1.Application{}, client.InNamespace(HASAppNamespace))
+		k8sClient.DeleteAllOf(context.Background(), &appstudiov1alpha1.Component{}, client.InNamespace(HASAppNamespace))
+		return nil
+	}
+
+	pact := &dsl.Pact{
+		Provider: "HAS",
+		LogLevel: "TRACE",
+	}
+
+	// Run pact tests
+	_, err = pact.VerifyProvider(t, verifyRequest)
 	if err != nil {
 		t.Errorf("Error while verifying tests. \n %+v", err)
 	}
@@ -158,5 +183,4 @@ func TestContracts(t *testing.T) {
 		fmt.Printf("%+v", err)
 		panic("Cleanup failed")
 	}
-
 }
