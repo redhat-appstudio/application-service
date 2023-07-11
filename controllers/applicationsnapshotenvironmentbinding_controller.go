e3acefd361256c0b6a71c97bb4361cb3e8793c67
John Collier <jcollier@redhat.com>
2023-07-11 09:29:27 -0400

    [RHTAPBUGS-322] Properly update SEB status
    Signed-off-by: John Collier <jcollier@redhat.com>

--- controllers/applicationsnapshotenvironmentbinding_controller.go
+++ controllers/applicationsnapshotenvironmentbinding_controller.go
@@ -158,7 +158,7 @@ func (r *SnapshotEnvironmentBindingReconciler) Reconcile(ctx context.Context, re
 	var tempDir string
 	clone := true
 
-	appSnapshotEnvBinding.Status.Components = []appstudiov1alpha1.BindingComponentStatus{}
+	//appSnapshotEnvBinding.Status.Components = []appstudiov1alpha1.BindingComponentStatus{}
 	for _, component := range components {
 		componentName := component.Name
 
@@ -385,7 +385,12 @@ func (r *SnapshotEnvironmentBindingReconciler) Reconcile(ctx context.Context, re
 			componentStatus.GitOpsRepository.GeneratedResources = componentGeneratedResources[componentName]
 		}
 
-		appSnapshotEnvBinding.Status.Components = append(appSnapshotEnvBinding.Status.Components, componentStatus)
+		for i := range appSnapshotEnvBinding.Status.Components {
+			if appSnapshotEnvBinding.Status.Components[i].Name == componentStatus.Name {
+				appSnapshotEnvBinding.Status.Components[i] = componentStatus
+				break
+			}
+		}
 
 		// Set the clone to false, since we dont want to clone the repo again for the other components
 		clone = false
