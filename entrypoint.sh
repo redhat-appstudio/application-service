67d31a01a7762c7ae060668f31ac4c22ea88604a
Kim Tsao <84398375+kim-tsao@users.noreply.github.com>
2023-07-17 14:44:17 -0400

    Remove feature flag (#365)
    Signed-off-by: Kim Tsao <ktsao@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -eux
 
-if [[ "$ENVIRONMENT" == "staging" ]]; then
-  echo "running tini"
-  exec tini -- /manager $*
-else
-  echo "running manager"
-  exec /manager $*
-fi
+echo "running manager under tini"
+exec tini -- /manager $*
+
