dc3a43b5d5a8f347aee9d30c4c2eb85a5d7e0756
Stephanie Cao <yangcao@redhat.com>
2023-09-21 11:00:28 -0400

    [DEVHAS-438]get latest Alizer (#376)
    * checkout latest Alizer
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * pull in latest release
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    ---------
    
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- cdq-analysis/pkg/componentdetectionquery.go
+++ cdq-analysis/pkg/componentdetectionquery.go
@@ -22,10 +22,9 @@ import (
 	"fmt"
 	"path"
 
-	"github.com/spf13/afero"
-
+	"github.com/devfile/alizer/pkg/apis/model"
 	"github.com/go-logr/logr"
-	"github.com/redhat-developer/alizer/go/pkg/apis/model"
+	"github.com/spf13/afero"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
