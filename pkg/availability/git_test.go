5a93359a19e35d9a77cb8697e53ab4ba998aa44e
Maysun J Faisal <maysunaneek@gmail.com>
2024-03-18 18:06:03 -0400

    Add in availability test
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

---
+++ pkg/availability/git_test.go
@@ -0,0 +1,41 @@
+//
+// Copyright 2024 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package availability
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/redhat-appstudio/application-service/pkg/github"
+	"github.com/redhat-appstudio/application-service/pkg/metrics"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckAvailability(t *testing.T) {
+
+	t.Run("check availability", func(t *testing.T) {
+		checkGitLabel := prometheus.Labels{"check": "github"}
+		r := AvailabilityWatchdog{
+			GitHubTokenClient: github.MockGitHubTokenClient{},
+		}
+
+		r.checkAvailability(context.TODO())
+
+		assert.Equal(t, 1.0, testutil.ToFloat64(metrics.HASAvailabilityGauge.With(checkGitLabel)))
+	})
+}
