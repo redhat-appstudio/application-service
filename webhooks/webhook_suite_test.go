b5a819969a33ee87439161fe743fedfa9fc04321
Ryan Cole <rcyoalne@gmail.com>
2024-11-05 13:05:00 -0500

    chore: update application-api version (#489)
    The component status CRD had a new field to it, but application-service
    would overwrite the field when anyone attempted to update it.  Updating
    application-service to the newest version of application-api fixes
    this issue
    
    Signed-off-by: Ryan Cole <rcyoalne@gmail.com>

--- webhooks/webhook_suite_test.go
+++ webhooks/webhook_suite_test.go
@@ -62,10 +62,10 @@ var _ = BeforeSuite(func() {
 	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
-	applicationAPIDepVersion := "v0.0.0-20231016183051-2dde965fce17"
+	applicationAPIDepVersion := "v0.0.0-20240812090716-e7eb2ecfb409"
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "redhat-appstudio", "application-api@"+applicationAPIDepVersion, "manifests")},
-		ErrorIfCRDPathMissing: false,
+		CRDDirectoryPaths:     []string{filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "konflux-ci", "application-api@"+applicationAPIDepVersion, "manifests")},
+		ErrorIfCRDPathMissing: true,
 		WebhookInstallOptions: envtest.WebhookInstallOptions{
 			Paths: []string{filepath.Join("..", "config", "webhook")},
 		},
@@ -74,6 +74,14 @@ var _ = BeforeSuite(func() {
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
+
+	//hasApplication := false
+	//for _, crd := range testEnv.CRDs {
+	//	if crd.Spec.Names.Singular == "Component" {
+	//		hasApplication = true
+	//	}
+	//}
+	//Expect(hasApplication).To(BeTrue())
 
 	scheme := runtime.NewScheme()
 	err = appstudiov1alpha1.AddToScheme(scheme)
